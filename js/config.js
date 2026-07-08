2e476d545e757724741c0aec4608e1031add306e
lovekaria2005 <lovekaria2005@gmail.com>
2026-07-08 22:40:01 +0530

    Add config for live deployment

---
+++ js/config.js
@@ -0,0 +1,5 @@
+export const API_KEY = "579b464db66ec23bdd000001a165f90412324a0a521ca957a8089e7b";
+export const RESOURCE_ID = "0764657f-00ec-4c6b-9ece-2d7b8a7401fa";
+export const BASE_URL = `https://api.data.gov.in/resource/${RESOURCE_ID}`;
+export const SUBMIT_URL = "https://jsonplaceholder.typicode.com/posts";
+export const RAW_FETCH_LIMIT = 150;
